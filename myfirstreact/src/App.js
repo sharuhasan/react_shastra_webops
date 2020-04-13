0ef7a26afeb9fd120d09b535b162a8fa0731080d
sharuhasan <62358367+sharuhasan@users.noreply.github.com>
2020-04-14 04:30:19 +0530

    Update App.js

--- myfirstreact/src/App.js
+++ myfirstreact/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     render() {
       return (
       <div className="App" >
+            <h2 style={{marginLeft:1178,paddingTop:120,marginBottom:0}}>REGISTRATION</h2>
             <VForm /> 
       </div>
       );
